1398d05d9e0a46906382c32e95d757b891fec1e0
Lrodriguez310 <W0812903@myscc.ca>
2022-11-21 21:52:13 -0500

    updated the tinyurl

--- LuisBooks.DataAccess/Repository/UnitOfWork.cs
+++ LuisBooks.DataAccess/Repository/UnitOfWork.cs
@@ -1,4 +1,47 @@
 ﻿using LuisBooks.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LuisBookStore.DataAccess.Data;
+
+namespace LuisBooks.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+            CoverType = new CoverTypeRepository(_db);
+            Product = new ProductRepository(_db);
+            SP_Call = new SP_Call(_db);
+        }
+
+        public ICategoryRepository Category { get; private set; }
+        public ICoverTypeRepository CoverType { get; private set; }
+        public IProductRepository Product { get; private set; }
+        public ISP_Call SP_Call { get; private set; }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
+
+
+
+
+/*using LuisBooks.DataAccess.Repository.IRepository;
 using LuisBookStore.DataAccess.Data;
 using System;
 using System.Collections.Generic;
@@ -39,20 +82,21 @@ public class UnitOfWork : IUnitOfWork //make the method public to access the cla
         _db.Dispose();
     }
 
-    /* public ICategoryRepository GetCoverType()
+    *//* public ICategoryRepository GetCoverType()
      {
          throw new NotImplementedException();
      }
-*/
+*//*
     public void Save()
     {
         _db.SaveChanges();  // All changes will be saved when the save method is called at the parent level
     }
 
-    /* public void save()
+    *//* public void save()
      {
          _db.SaveChanges();
         // throw new NotImplementedException();
-     }*/
+     }*//*
 }
     }
+*/
