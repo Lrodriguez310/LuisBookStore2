2ff3736b946e7551be2aa05141b32e97a352fa4f
Lrodriguez310 <W0812903@myscc.ca>
2022-11-21 21:08:14 -0500

    installed Microsoft.aspnetcore.movs.rendering in productvm.cs

---
+++ LuisBooks.Models/ViewModels/ProductVM.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LuisBooks.Models.ViewModels
+{
+    public class ProductVM
+    {
+        public Product Product { get; set; } //Displays all the products details
+
+        public IEnumerable<SelectListItem> CategoryList { get; set; } //install the pakage "MS. Aspcore.Mvc.Rendering"
+        public IEnumerable<SelectListItem> CoverTypeList { get; set; } //install the pakage "MS. Aspcore.Mvc.Rendering"
+
+    }
+}
